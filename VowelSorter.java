352d5de4ac175715373742e52fe367916c02c507
Andry925 <andry925@ukr.net>
2024-10-20 15:29:22 +0300

    Add method to join SortedWords, finish implementing main method

--- VowelSorter.java
+++ VowelSorter.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class VowelSorter {
 
-
     public static int countVowels(StringBuffer word) {
         int count = 0;
         String vowels = "aeiouAEIOU";
@@ -15,17 +14,17 @@ public class VowelSorter {
         return count;
     }
 
-   public static int countWordsInText(StringBuffer text ){
+    public static int countWordsInText(StringBuffer text) {
         int wordCount = 1;
-        for (int i = 0; i < text.length(); i ++){
-            if (text.charAt(i) == ''){
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
                 wordCount += 1;
             }
         }
-        return wordCount
-   }
+        return wordCount;
+    }
 
-   public static StringBuffer[] splitGivenText(StringBuffer text, int wordCount){
+    public static StringBuffer[] splitGivenText(StringBuffer text, int wordCount) {
         StringBuffer[] wordsInBuffer = new StringBuffer[wordCount];
         int currentWordIndex = 0;
         wordsInBuffer[currentWordIndex] = new StringBuffer();
@@ -42,10 +41,10 @@ public class VowelSorter {
         return wordsInBuffer;
     }
 
+    
     public static void sortWordsByVowelAmount(StringBuffer[] words) {
-
-        for (int i = 0; i < words.length - 1; i++){
-            for (int j = 0; j < words.length -1 -i; j++) {
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = 0; j < words.length - 1 - i; j++) {
                 if (countVowels(words[j]) > countVowels(words[j + 1])) {
                     StringBuffer temp = words[j];
                     words[j] = words[j + 1];
@@ -54,5 +53,31 @@ public class VowelSorter {
             }
         }
     }
+
+    
+    public static StringBuffer joinSortedWordsBack(StringBuffer[] words) {
+        StringBuffer result = new StringBuffer();
+        for (int i = 0; i < words.length; i++) {
+            result.append(words[i]);
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Text that needs to be sorted ");
+        String text = scanner.nextLine();
+
+        StringBuffer textGiven = new StringBuffer(text);
+        int wordCount = countWordsInText(textGiven);
+        StringBuffer[] words = splitGivenText(textGiven, wordCount);
+
+        sortWordsByVowelAmount(words);
+        StringBuffer sortedText = joinSortedWordsBack(words);
+
+        System.out.println("Text that is sorted: " + sortedText); 
+    }
 }
-        
